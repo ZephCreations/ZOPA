bcb0cb6a5e106fdba4dba6a9914289a633339db6
ZephCreations <STEM-futur3@outlook.com>
2020-07-13 17:23:35 +1000

    Modified models.py to create a model 'Event'. This will control user created Events

--- ZOPA/events/models.py
+++ ZOPA/events/models.py
@@ -1,3 +1,21 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Event(models.Model):
+    event_text = models.CharField(max_length=200)
+    start_date = models.DateTimeField('start date/time')
+    end_date = models.DateTimeField('end date/time')
+
+    def has_started(self):
+        now = timezone.now()
+        return now < self.end_date > self.start_date
+
+    def has_finished(self):
+        now = timezone.now()
+        return now > self.end_date
+
+    def __str__(self):
+        return self.event_text
