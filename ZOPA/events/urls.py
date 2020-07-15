c83c0066951900a9b128ba44e4b2fe77fcf4ae8a
ZephCreations <STEM-futur3@outlook.com>
2020-07-15 16:21:21 +1000

    Added templates and modified settings.py, views.py and urls.py

--- ZOPA/events/urls.py
+++ ZOPA/events/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:event_id>/', views.detail, name='detail'),
+    path('<int:event_id>/edit/', views.edit, name='edit')
 ]
--- ZOPA/events/views.py
+++ ZOPA/events/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from .models import Event
 # Create your views here.
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the events index.")
+    upcoming_events_list = Event.objects.order_by('-start_date')[:5]
+    context = {'upcoming_events_list': upcoming_events_list}
+    return render(request, 'events/index.html', context)
+
+def detail(request, event_id):
+    event = get_object_or_404(Event, pk=event_id)
+
+
+    return render(request, 'events/detail.html', {'event': event})
+
+def edit(request, event_id):
+    response = "You're editing the event '%s'"
+    return HttpResponse(response % event_id)
