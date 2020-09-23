49489450af60cf4100cba2f1ecc7aa65996696be
ZephCreations <stem-futur3@outlook.com>
2020-09-23 13:51:52 +1000

    Added Repeat value to models.py

--- ZOPA/events/models.py
+++ ZOPA/events/models.py
@@ -6,9 +6,20 @@ from django.utils import timezone
 
 # Create your models here.
 class Event(models.Model):
+    REPEAT_BY_CHOICES = [
+        ('D', 'Daily'),
+        ('W', 'Weekly'),
+        ('F', 'Fortnightly'),
+        ('M', 'Monthly'),
+        ('Y', 'Yearly'),
+    ]
+
     event_text = models.CharField(max_length=200)
     start_date = models.DateTimeField('start date/time')
     end_date = models.DateTimeField('end date/time')
+    repeat_by = models.CharField('Repeat event', blank=True, max_length=1, choices=REPEAT_BY_CHOICES)
+
+
 
     def has_started(self):
         now = timezone.now()
